ade0a89c4c9b1818466d2fc5a2b8c8acd7c17a9a
Vasiliy Ermolovich <younash@gmail.com>
2012-09-14 09:21:16 +0300

    add replace-neg function

--- src/clojure_exercises/basics.clj
+++ src/clojure_exercises/basics.clj
@@ -2,3 +2,8 @@
 
 (defn shift-pop [items]
   (rest (butlast items)))
+
+(defn replace-negs [items]
+  (map (fn [n]
+            (if (neg? n) 0 n))
+        items))
--- test/clojure_exercises/basics_test.clj
+++ test/clojure_exercises/basics_test.clj
@@ -5,3 +5,7 @@
 (deftest shift-pop-test
   (testing "Returns a new list without first and last items"
     (is (= (shift-pop '(1 2 3 4 5)) '(2 3 4)))))
+
+(deftest replace-negs-test
+  (testing "Returns a new list without negative items"
+    (is (= (replace-negs '(1 -2 3 -4 5)) '(1 0 3 0 5)))))
